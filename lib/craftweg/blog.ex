2846591efa67f770dd8f441bbca49bd0fb051691
Pedro Piñera <pedro@ppinera.es>
2022-09-11 07:56:25 +0200

    Comment out tests

--- lib/craftweg/blog.ex
+++ lib/craftweg/blog.ex
@@ -11,7 +11,7 @@ defmodule Craftweg.Blog do
 
   @posts Enum.sort_by(@posts, & &1.date, {:desc, Date})
   @categories @posts |> Enum.flat_map(& &1.categories) |> Enum.uniq() |> Enum.sort()
-  @feed_xml @posts |> Enum.map(& &1)
+  @feed @posts |> Craftweg.Feed.generate
 
   @doc """
   The function returns all the Craftweg.Blog.Post posts.
@@ -22,8 +22,4 @@ defmodule Craftweg.Blog do
   The function returns all the categories.
   """
   def all_categories, do: @categories
-
-  def feed_xml() do
-    []
-  end
 end
---
+++ lib/craftweg/feed.ex
@@ -0,0 +1,5 @@
+defmodule Craftweg.Feed do
+  def generate(posts) do
+    []
+  end
+end
