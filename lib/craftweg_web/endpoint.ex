a00bdaf5f372e16ee3e3ec088b2e537533b2a9de
Pedro Piñera <pedro@ppinera.es>
2022-09-11 20:03:48 +0200

    Add favicon

--- lib/craftweg_web/endpoint.ex
+++ lib/craftweg_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule CraftwegWeb.Endpoint do
     at: "/",
     from: :craftweg,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts favicon images favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
