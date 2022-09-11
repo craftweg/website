62d26fc40b881675381016212640f209e3c1cb85
Pedro Piñera <pedro@ppinera.es>
2022-09-11 21:32:29 +0200

    Fix deployment

--- lib/craftweg/blog.ex
+++ lib/craftweg/blog.ex
@@ -7,7 +7,7 @@ defmodule Craftweg.Blog do
     from: Application.app_dir(:craftweg, "priv/posts/**/*.md"),
     as: :posts,
     parser: Parser,
-    highlighters: [:makeup_elixir, :makeup_erlang]
+    highlighters: [:makeup_ruby, :makeup_typescript, :makeup_elixir, :makeup_javascript, :makeup_swift, :makeup_erlang, :makeup_objc]
 
   @posts Enum.sort_by(@posts, & &1.date, {:desc, Date})
   @categories @posts |> Enum.flat_map(& &1.categories) |> Enum.uniq() |> Enum.sort()
