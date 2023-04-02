e1ae259fb02c80f8e929b067dc751ac323b61227
Pedro Piñera <pedro@craftweg.com>
2023-04-02 17:45:10 +0200

    Improve Nix setup

--- default.nix
+++ default.nix
@@ -1,9 +1,23 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
 let
-  pkgs = import <nixpkgs> { };
+  inherit (lib) optional optionals;
+  elixir = beam.packages.erlangR25.elixir_1_14;
 in
 pkgs.mkShell {
   buildInputs = [
-    pkgs.elixir_1_14
-    pkgs.erlangR25
-  ];
+    pkgs.flyctl
+    pkgs.nodejs
+    elixir
+  ]
+  ++ optional stdenv.isLinux libnotify # For ExUnit Notifier on Linux.
+  ++ optional stdenv.isLinux inotify-tools # For file_system on Linux.
+  ++ optional stdenv.isDarwin terminal-notifier # For ExUnit Notifier on macOS.
+  ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+    # For file_system on macOS.
+    CoreFoundation
+    CoreServices
+  ]);
 }
