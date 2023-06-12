ba57d202f5dee69c3cefcb2bd0f55a3abdab543e
Pedro Piñera <pedro@pepicrft.me>
2023-06-12 17:02:13 +0200

    Format

--- lib/pepicrft_web/plugs/attack_plug.ex
+++ lib/pepicrft_web/plugs/attack_plug.ex
@@ -6,8 +6,10 @@ defmodule PepicrftWeb.Plugs.AttackPlug do
   end
 
   rule "throttle by ip", conn do
-    throttle conn.remote_ip,
-      period: 60_000, limit: 10,
+    throttle(conn.remote_ip,
+      period: 60_000,
+      limit: 10,
       storage: {PlugAttack.Storage.Ets, PepicrftWeb.Plugs.AttackPlug.Storage}
+    )
   end
 end
