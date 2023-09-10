6fa3763f5662143d2ba740c950402d544b526348
Pedro <pedro@pepicrft.me>
2023-09-10 16:36:23 +0200

    Remove Patreon

--- lib/pepicrft_web/controllers/feed_xml.ex
+++ lib/pepicrft_web/controllers/feed_xml.ex
@@ -10,16 +10,4 @@ defmodule PepicrftWeb.FeedXML do
     |> Timezone.convert("GMT")
     |> Timex.format!("{RFC3339}")
   end
-
-  def just_for_fun_sign_up_html_footer do
-    """
-    <h2>Subscribe to my newsletter</h2>
-    <p>
-      <b>Just for Fun</b> is a more intimate space where I share in-depth experiences and lessons learned from crafting software and making a living out of it. It's sent every Saturday morning.
-    </p>
-    <p>
-      <a href="https://justforfun.pepicrft.me/signup">Subscribe</a>
-    </p>
-    """
-  end
 end
